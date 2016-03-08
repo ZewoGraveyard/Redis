4be9469733d8b85e0d56dc5ac3f74354d3172b47
Ricardo Borelli <ricardo.abc@gmail.com>
2016-03-08 15:35:03 -0300

    Name change

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "ZRedis",
+    name: "Redis",
     dependencies: [
         .Package(url: "https://github.com/Zewo/TCP.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 2),
