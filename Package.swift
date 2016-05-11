46b012865e1c7e397a91dc57b1b7e2cc09059585
Denis Kilin <kilin@starline.ru>
2016-05-11 14:23:01 +0300

    Updating to 05.03 snapshot

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Redis",
     dependencies: [
-        .Package(url: "https://github.com/VeniceX/TCP.git", majorVersion: 0, minor: 6)
+        .Package(url: "https://github.com/VeniceX/TCP.git", majorVersion: 0, minor: 7)
     ]
 )
