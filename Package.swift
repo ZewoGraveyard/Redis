68bb2f677095f4e45b87d92918af0d22d3578ff8
kiokumicu <kiokumicu@gmail.com>
2016-06-09 16:56:32 +0300

    Update to 05-03 snapshot
    * Updating to 04-25 snapshot
    
    * Updating to 05.03 snapshot

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Redis",
     dependencies: [
-        .Package(url: "https://github.com/VeniceX/TCP.git", majorVersion: 0, minor: 5)
+        .Package(url: "https://github.com/VeniceX/TCP.git", majorVersion: 0, minor: 7)
     ]
 )
--- Sources/Parser.swift
+++ Sources/Parser.swift
@@ -3,7 +3,7 @@ extension String {
 
     public func indexOf(character char: Character) -> Int? {
         if let idx = self.characters.index(of: char) {
-            return self.startIndex.distance(to: idx)
+            return self.distance(from: self.startIndex, to: idx)
         }
         return nil
     }
@@ -21,7 +21,7 @@ struct Parser {
 	static func readResponse(_ fullResponse: String) throws -> Any? {
 
 		let byte: Character = fullResponse[fullResponse.startIndex]
-		let response: String = fullResponse[fullResponse.startIndex.advanced(by: 1)..<fullResponse.endIndex]
+    let response: String = fullResponse[fullResponse.index(fullResponse.startIndex, offsetBy: 1)..<fullResponse.endIndex]
 
 		var result: Any?
 
@@ -34,18 +34,18 @@ struct Parser {
 		case ":":
 			// Simple integer
 			let idx = Int(response.indexOf(character: "\r\n")!)
-			result = Int(response[response.startIndex..<response.startIndex.advanced(by: idx)])
+			result = Int(response[response.startIndex..<response.index(response.startIndex, offsetBy: idx)])
 		case "+":
 			// Simple scleng
 			result = String(response)
 		case "$":
 			// Bulk string
 			let idx = Int(response.indexOf(character: "\r\n")!)
-			if response[response.startIndex..<response.startIndex.advanced(by: idx)] == "-1" {
+			if response[response.startIndex..<response.index(response.startIndex, offsetBy: idx)] == "-1" {
 				// nil string
 				result = nil
 			} else {
-				result = String(response[response.startIndex.advanced(by: idx)..<response.endIndex])
+				result = String(response[response.index(response.startIndex, offsetBy: idx)..<response.endIndex])
 			}
 
 		case "*":
@@ -61,7 +61,7 @@ struct Parser {
 
 				while values.count > 0 {
 					var tmp: [Any?] = []
-					let tail: Int = Int(String(values[0][values[0].startIndex.advanced(by: 1)]))!
+          let tail: Int = Int(String(values[0][values[0].index(values[0].startIndex, offsetBy: 1)]))!
 
 					values.remove(at: 0)
 					while tmp.count < tail {
