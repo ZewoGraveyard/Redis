eccf8376aa21f3586e57e2526556be3416bca955
✪ vi4m Marcin Kliks <vi4m@users.noreply.github.com>
2016-08-26 19:07:31 +0200

    Fix Socket is not connected error
    Add conn.open() method while initializing Redis() object

--- Sources/Redis.swift
+++ Sources/Redis.swift
@@ -5,6 +5,7 @@ public final class Redis: Commands {
 	public var conn: TCPConnection
 
 	public init(_ host: String, _ port: Int) throws {
-        conn = try TCPConnection(host: host, port: port)
+        	conn = try TCPConnection(host: host, port: port)
+        	try conn.open()
 	}
 }
